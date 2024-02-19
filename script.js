abde3f33530bb30cc3cce471dcc203693cc689f1
xlxk-of <83321108+xlxk-of@users.noreply.github.com>
2024-02-19 07:07:07 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,82 +1,118 @@
-var canvas = document.querySelector('#c'),
-	ctx = canvas.getContext('2d'),
-	points = [],
-	m = {x: null, y: null},
-	r = 0;
-
-var a = 20; // how many dots to have
-var b = 5; // how fast to spin
-var c = 0.1; // how much to fade. 1 all, 0.5 half, 0 none
-var d = 100; // distance from the mouse
-
+const canvas = document.getElementById('trailCanvas');
+const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-m.x = canvas.width / 2;
-m.y = canvas.height / 2;
+let particlesArray = [];
+let lastMousePos = { x: 0, y: 0 };
+let lastMouseAngle = 0;
+let mouseSpeed = 0;
 
-window.addEventListener('mousemove', function(e){
-	TweenMax.to(m, 0.3, {x: e.clientX, y: e.clientY, ease: 'linear'})
-	document.querySelector('.message').className = 'hide';
-})
+const colors = [
+  { color: "#003290", scalingFactor: 0.94, size: () => 18, attractionFactor: -0.0005, maxStretch: 3, directionRandomness: 0.3 },
+  { color: "#0097b5", scalingFactor: 0.94, size: () => 12, attractionFactor: 0.0003, maxStretch: 5, directionRandomness: 0.15 },
+  { color: "#ffffff", scalingFactor: 0.93, size: () => 7, attractionFactor: 0.001, maxStretch: 7, directionRandomness: 0 }
+];
 
-for(var i=0;i<a;i++){
-	points.push({
-		r: 360 / a * i,
-		p: {x: null, y: null},
-		w: Math.random()*5,
-		c: '#fff',
-		d: Math.random() * (d + 5) - 5,
-		s: Math.random() * (b + 5) - 5
-	})
+function calculateMouseSpeed(e) {
+  const dx = e.x - lastMousePos.x;
+  const dy = e.y - lastMousePos.y;
+  mouseSpeed = Math.sqrt(dx * dx + dy * dy);
+  lastMouseAngle = Math.atan2(e.y - lastMousePos.y, e.x - lastMousePos.x)
+  lastMousePos = { x: e.x, y: e.y };
 }
 
-function render(){
-	if(m.x == null || m.y == null) return;
+class Particle {
+  constructor(x, y, speed, directionAngle, stretchFactor, { color, scalingFactor, size, maxStretch, directionRandomness }) {
+    this.x = x;
+    this.y = y;
+    const isRandom = Math.random() < directionRandomness;
+    this.color = color;
+    this.size = size();
+    this.speed = speed;
+    this.directionAngle = isRandom ? (Math.random() * Math.PI * 2) : directionAngle;
+    this.directionX = Math.cos(this.directionAngle) * this.speed;
+    this.directionY = Math.sin(this.directionAngle) * this.speed;
+    this.stretchFactor = Math.min(isRandom ? 0 : stretchFactor, maxStretch);
+    this.scalingFactor = scalingFactor;
+  }
 
-	ctx.fillStyle = 'rgba(0,0,0,'+c+')';
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+  update() {
+    this.x += this.directionX;
+    this.y += this.directionY;
+    if (this.size > 1) this.size *= this.scalingFactor;
+  }
 
-	ctx.lineCap = 'round';
-
-	for(var i=0; i<points.length; i++){
-		var p = points[i];
-
-		p.r += p.s;
-		if(p.r >= 360) p.r = p.r - 360;
-
-		var vel = {
-			x: p.d * Math.cos(p.r * Math.PI / 180),
-			y: p.d * Math.sin(p.r * Math.PI / 180)
-		};
-
-		if(p.p.x != null && p.p.y != null){
-			ctx.strokeStyle = p.c;
-			ctx.lineWidth = 2;
-			ctx.beginPath();
-			ctx.moveTo(p.p.x, p.p.y);
-			ctx.lineTo(m.x + vel.x, m.y + vel.y)
-			ctx.stroke();
-			ctx.closePath();
-		}
-
-		p.p.x = m.x + vel.x;
-		p.p.y = m.y + vel.y;
-	}
+  draw() {
+    ctx.beginPath();
+    ctx.ellipse(
+      this.x - Math.cos(this.directionAngle) * this.size,
+      this.y - Math.sin(this.directionAngle) * this.size,
+      this.size * this.stretchFactor,
+      this.size,
+      this.directionAngle,
+      0,
+      Math.PI * 2
+    );
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
 }
 
+function createParticles(e) {
+  calculateMouseSpeed(e);
 
-window.requestAnimFrame = (function(){
-return  window.requestAnimationFrame   ||
-	window.webkitRequestAnimationFrame ||
-	window.mozRequestAnimationFrame    ||
-	function(callback){
-		window.setTimeout(callback, 1000 / 60);
-	};
-})();
+  const xPos = e.x;
+  const yPos = e.y;
+  const numberOfParticlesPerColor = 1;
+  const stretchFactor = Math.max(1, mouseSpeed / 2);
 
-(function animloop(){
-	requestAnimFrame(animloop);
-	render();
-})();
+  for (let palette of colors) {
+    for (let i = 0; i < numberOfParticlesPerColor; i++) {
+      const speed = Math.random() * 1 + 0.5;
+      const directionAngle = lastMouseAngle;
+      particlesArray.push(
+        new Particle(xPos, yPos, speed, directionAngle, stretchFactor, palette)
+      );
+    }
+  }
+}
+
+function handleParticles() {
+  for (let palette of colors) {
+    for (let i = 0; i < particlesArray.length; i++) {
+      if (particlesArray[i].color === palette.color) {
+        const dx = lastMousePos.x - particlesArray[i].x;
+        const dy = lastMousePos.y - particlesArray[i].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        particlesArray[i].directionX += dx * palette.attractionFactor;
+        particlesArray[i].directionY += dy * palette.attractionFactor;
+
+        particlesArray[i].update();
+        particlesArray[i].draw();
+
+        if (particlesArray[i].size <= 2) {
+          particlesArray.splice(i, 1);
+          i--;
+        }
+      }
+    }
+  }
+}
+
+let i = 0
+const fps = 60
+let lastRender = 0
+function animate() {
+  if ((Date.now() - lastRender) > (1000 / fps)) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    lastRender = Date.now()
+  }
+  requestAnimationFrame(animate);
+}
+
+canvas.addEventListener('mousemove', createParticles);
+animate();
